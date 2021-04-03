818b7042cf6eef4e31a3d629cb2e03ce309bee3c
Aigerim98 <aigerim.abdurahmanova@gmail.com>
2021-04-03 20:07:21 +0100

    call back and threads added

--- TestedDevices/DHT11/DHT11.cpp
+++ TestedDevices/DHT11/DHT11.cpp
@@ -1,59 +1,89 @@
 #include "DHT11.h"
 #include <wiringPi.h>
 
-void DHT11::set_pin(int pin){
-	DHT11::DHTPIN = pin;
+DHT11::DHT11(int pin){
+	wiringPiSetup();
+	DHT11::set_pin(pin);
 }
 
-void DHT11::read_dht_data(){
+void DHT11::set_pin(int pin){
+	DHTPIN = pin;
+}
+
+bool DHT11::read_dht_data(){
 	uint8_t laststate	= HIGH;
 	uint8_t counter		= 0;
 	uint8_t j		= 0, i;
-	float	f; 
-	int dht11_dat[5] = { 0, 0, 0, 0, 0 };
 	dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
  
-	pinMode(DHT11::DHTPIN, OUTPUT );
-	digitalWrite(DHT11::DHTPIN, LOW );
+	//pull pin down for 18 milliseconds
+	pinMode(DHTPIN, OUTPUT);
+	digitalWrite(DHTPIN, LOW);
 	delay( 18 );
-	digitalWrite(DHT11::DHTPIN, HIGH );
-	delayMicroseconds( 40 );
-	pinMode(DHT11::DHTPIN, INPUT );
- 
-	for ( i = 0; i < DHT11::MAXTIMINGS; i++ )
+	
+	digitalWrite(DHTPIN, HIGH);
+	delayMicroseconds(40);
+	
+	//prepare to read the pin
+	pinMode(DHTPIN, INPUT);
+	
+	//detect change and read data
+	for ( i = 0; i < MAXTIMINGS; i++ )
 	{
 		counter = 0;
-		while ( digitalRead(DHT11::DHTPIN ) == laststate )
+		while ( digitalRead(DHTPIN) == laststate )
 		{
 			counter++;
-			delayMicroseconds( 1 );
+			delayMicroseconds(1);
 			if ( counter == 255 )
 			{
 				break;
 			}
 		}
-		laststate = digitalRead(DHT11::DHTPIN );
+		laststate = digitalRead(DHTPIN);
  
 		if ( counter == 255 )
 			break;
  
-		if ( (i >= 4) && (i % 2 == 0) )
+		if ( (i >= 4) && (i % 2 == 0) ) //ignore first three transitions 
 		{
 			dht11_dat[j / 8] <<= 1;
-			if ( counter > 55 )
-				dht11_dat[j / 8] |= 1;
+			if ( counter > 30 )
+				dht11_dat[j / 8] |= 1; //combine bits into bytes
 			j++;
 		}
 	}
- 
-	if ( (j >= 40) &&
-	     (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
-	{
-		f = dht11_dat[2] * 9. / 5. + 32;
-		printf( "Humidity = %d.%d %% Temperature = %d.%d C (%.1f F)\n",
-			dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f );
-	}else  {
-		printf( "Data not good, skip\n" );
+		//check sensor was read 40 bits and verify checksum in the last byte
+		if ( (j >= 40) &&
+			 (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
+		{
+			return true;
+		}else{
+			return false;
+		}	
+}
+void DHT11::setCallBack(DHT11callback* cb){
+	dht11callback = cb;
+}
+void DHT11::run(DHT11* dht11){
+	dht11->running = 1;
+	while(dht11->running){
+		if(dht11->read_dht_data()){
+			if(dht11->dht11callback){
+				dht11->dht11callback->hasSample(dht11->dht11_dat);
+			}
+		}
 	}
-	
+}
+void DHT11::start(){
+	if(dhtThread) throw "called while dhtThread is running";
+	dhtThread = new std::thread(run, this);
+}
+void DHT11::stop(){
+	running = 0;
+	if(dhtThread){
+		dhtThread->join();
+		delete dhtThread;
+		dhtThread = NULL;
 	}
+}
--- TestedDevices/DHT11/main.cpp
+++ TestedDevices/DHT11/main.cpp
@@ -4,18 +4,36 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-int main(void){
-	printf( "Raspberry Pi wiringPi DHT11 Temperature test program\n" );
-	DHT11* dht11 = new DHT11();
-	int pin = 7;
-	dht11->set_pin(pin);
+#include <string.h>
+#include <errno.h>
+#include <math.h>
+#include <time.h>
+#include <thread>
+
+class DHT11SampleCallBack:public DHT11callback{
+	//call back function that calculates humidity and temperature
+	//from bits
+	virtual void hasSample(int *data){
+		float humidity = (float) ((data[0] << 8) + data[1]) / 10.0;
+		if (humidity > 100) humidity = data[0];
+		float temperature = (float)(((data[2] & 0x7F) << 8) + data[3]) / 10;
+		if (temperature > 125) temperature = data[2];
+		if (data[2] & 0x80) temperature = -temperature;
+		float f_temperature = temperature * 9. / 5. + 32;
+		printf("Humidity = %.1f %% Temperature = %.1f *C (%.1f *F)\n", humidity, temperature, f_temperature); 
+	}
+};
+
+int main(int argc, char *argv[]){
 	if ( wiringPiSetup() == -1 )
 		exit( 1 );
- 
-	while ( 1 )
-	{
-		dht11->read_dht_data();
-		delay( 2000 ); 
-	}
+	int pin = 7;
+	DHT11* dht11 = new DHT11(pin);
+	DHT11SampleCallBack cb;
+	dht11->setCallBack(&cb);
+	dht11->start();
+	getchar();
+	dht11->stop();
+	delete dht11;
 	return 0;
 }
